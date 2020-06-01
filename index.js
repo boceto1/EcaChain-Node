1b89d8fcc9d0ef9e2b29502e8ad87032b2d1832d
boceto1 <janka.obando@outlook.es>
2020-06-01 06:03:47 -0500

    spreadChain

--- index.js
+++ index.js
@@ -16,8 +16,15 @@ app.get('/api/blocks', (req, res) => {
 app.post('/api/mine', (req, res) => {
   const { data } = req.body;
   blockchain.addBlock({ data });
+  pubsub.broadcastChain();
   res.redirect('/api/blocks');
 });
 
-const PORT = 5000;
+const DEFAULT_PORT = 5000;
+let PEER_PORT;
+if (process.env.GENERATE_PEER_PORT === 'true'){
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => console.log(`listening at localhost: ${PORT}`));
