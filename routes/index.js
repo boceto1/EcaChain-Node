53a009c74db08741132177a93ffeab38ee57e751
diego40g <diegopaznaula@hotmail.com>
2022-10-02 21:29:21 -0500

    (getPersonTransactions) Documentation of endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,26 @@
+/**
+ * Llamado de la libreria express utilizado para usar dentro de las peticiones HTTP
+ */
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
+/**
+ * Implementacion de bodyParser que permite el uso de application/json en el body de las peticiones
+ */
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+/**
+ * Rutas de acceso para endpoints de generacion de bloques, transacciones y datos de la blockchain
+ */
 app.use('/blocks', require('./block.routes'));
 app.use('/transactions', require('./transaction.routes'));
 app.use('/pool', require('./transactionPool.routes'));
 app.use('/data/transactions', require('./transactions.routes'));
 
+/**
+ * Ruta de acceso para los endpoints de usuarios obtenidos desde la base de datos
+ */
 app.use('/data/user', require('./userdata.routes'));
 
 module.exports = app;
--- routes/userdata.routes.js
+++ routes/userdata.routes.js
@@ -1,11 +1,24 @@
+/**
+ * Rutas que llaman al controlador de los usuarios para mostrar la información
+ * Librerias utilizadas para llamar a peticiones HTTP
+ */
 const express = require('express');
 const app = express();
 
+/**
+ * Metodos importados desdel el controlador userData
+ */
 const {
-getPersonTransaction,
+    getPersonTransaction,
 } = require('../controller/userData.controller');
 
-
-app.post('', getPersonTransaction);
+/**
+ * Peticion post para obtener las transacciones mediante el webId
+ * URL: {{URL_SERVER}}/api/data/user/getTransaction
+ * BODY: {
+ *          "user":"https://dipaz.inrupt.net/profile/card#me"
+ *       }
+ */
+app.post('/getTransaction', getPersonTransaction);
 
 module.exports = app;
