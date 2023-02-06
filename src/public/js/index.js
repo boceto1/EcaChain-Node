a53b90df7296116e46eb88902995ab4f475b585a
boceto1 <janka.obando@outlook.es>
2023-02-06 06:38:20 -0500

    display transactionInformation

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,10 +1,29 @@
-
+const TRANSACTION_ID_SELECTOR = '.details-box-id > p';
+const TRANSACTION_INFORMATION_SELECTOR = '.details-box-information > textarea';
 
 function seemoreTransaction (transactionId) {
   fetch(`/api/data/transactions/${transactionId}`)
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => fillTransactionInformation(transactionId, data))
     .catch((error) => {
       console.error('Error:', error);
     });
+}
+
+function clearFields () {
+  [TRANSACTION_ID_SELECTOR, TRANSACTION_INFORMATION_SELECTOR]
+    .forEach(selector => {
+      const element = document.querySelector(selector);
+      element.textContent = '';
+    })
+};
+
+function fillTransactionInformation (transactionId, transactionData) {
+  const idElement = document.querySelector(TRANSACTION_ID_SELECTOR);
+  const transactionElement = document.querySelector(TRANSACTION_INFORMATION_SELECTOR);
+
+  clearFields();
+
+  idElement.textContent = transactionId;
+  transactionElement.textContent = JSON.stringify(transactionData);
 }
