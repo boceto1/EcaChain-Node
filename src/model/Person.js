82c273667c59e9a554681110195dac372782162e
diego40g <diegopaznaula@hotmail.com>
2023-03-01 10:35:55 -0500

    deleting js files don't used into app

--- src/model/Person.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const PersonSchema = new Schema ({
-    userId: { type: String, require: true },
-    firstName: { type: String, require: false},
-    lastName: {type: String, require: false},
-});
-
-module.exports = model('Person', PersonSchema);
