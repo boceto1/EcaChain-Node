877e0e8956e7666046888ebf2f4ec8cebaee9f24
diego40g <diegopaznaula@hotmail.com>
2023-03-01 12:00:23 -0500

    correct path of model and type in fucntion

--- src/controller/block.controller.ts
+++ src/controller/block.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
-import Block from '../model/typescript/Block';
+import Block from '../model/Block';
 import SigletonElements from '../singleton/singleton';
 
 const blockchain = SigletonElements.getBlockchain();
 const pubsub = SigletonElements.getPubSub();
 
-export const getBlocks = async (_: any, res: Response) => {
+export const getBlocks = async (_: Request, res: Response) => {
   try {
     const blocks = await Block.find();
     return res.status(200).json(blocks);
